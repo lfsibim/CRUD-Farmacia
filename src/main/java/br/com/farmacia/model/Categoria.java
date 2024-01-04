5f1c4cfd9bf9c2c46ccc72ca3f9154331003b581
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2024-01-04 10:57:50 -0300

    add categoria model e repository

---
+++ src/main/java/br/com/farmacia/model/Categoria.java
@@ -0,0 +1,41 @@
+package br.com.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Tipo é obrigatório!")
+	@Size(max = 100, message = "O Tipo deve ter no máximo 100 caracteres!")
+	@Column(length = 100)
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
