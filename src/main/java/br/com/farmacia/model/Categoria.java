cf102459f017a62468b620765b646bc3575fd44b
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2024-01-04 12:34:28 -0300

    add model produto + relacionamento com categoria

--- src/main/java/br/com/farmacia/model/Categoria.java
+++ src/main/java/br/com/farmacia/model/Categoria.java
@@ -1,10 +1,17 @@
 package br.com.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -21,6 +28,10 @@ public class Categoria {
 	@Size(max = 100, message = "O Tipo deve ter no máximo 100 caracteres!")
 	@Column(length = 100)
 	private String tipo;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -36,6 +47,14 @@ public class Categoria {
 
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 }
---
+++ src/main/java/br/com/farmacia/model/Produto.java
@@ -0,0 +1,105 @@
+package br.com.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O Atributo Nome é Obrigatório!")
+	@Size(max = 100, message = "O Nome deve ter no máximo 100 caracteres!")
+	@Column(length = 100)
+	private String nome;
+	
+	@NotBlank(message = "O Atributo Marca é Obrigatório!")
+	@Size(max = 100, message = "A Marca deve ter no máximo 50 caracteres!")
+	@Column(length = 50)
+	private String marca;
+	
+	@Size(max = 200, message = "A Descrição deve ter no máximo 200 caracteres!")
+	@Column(length = 200)
+	private String descricao;
+	
+	@NotNull(message = "A Quantidade em Estoque é Obrigatório!")
+	private int estoque;
+	
+	@NotNull(message = "O Preço do Produto é Obrigatório!")
+	private float preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}	
+	
+	
+}
